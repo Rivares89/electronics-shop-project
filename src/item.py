c7c3e3b75dc585e206f9b872ae47e8d1b157f9f5
Aleksandr Buganov <rivares89@gmail.com>
2023-10-08 22:29:39 +0300

    instantiate_from_csv

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,14 +15,28 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
 
+        @property
+        def get_name(self):
+            '''геттер для name'''
+            return self.__name
 
-    def calculate_total_price(self, price: float, quantity: int) -> float:
+        @get_name.setter
+        def setter_name(self, __name):
+            if len(__name) <= 10:
+                return __name
+            else:
+                return __name[:10]
+
+
+
+
+    def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
@@ -28,14 +44,23 @@ class Item:
         """
         return self.price * self.quantity
 
-    def apply_discount(self, self.price, self.pay_rate) -> None:
+    def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
         return self.price
 
-# item1 = Item()
-# item2 = Item()
-#     all.append(item1)
-#     all.append(item2)
+
+    # csv_file = 'src/item.csv'
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        items = []
+        with open(csv_file, mode='r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+            for row in csv_reader:
+                name, price, quantity = row
+                item = cls(name, float(price), int(quantity))
+                items.append(item)
+        return items
